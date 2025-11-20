b00cc09987e22a5036e5e7b1ba3c83bc1f92a9d4
Blaine Motsinger <blaine@renderorange.com>
2025-11-19 21:57:26 -0500

    GH-8: add error modal for message handling

--- public/js/message.js
+++ public/js/message.js
@@ -15,24 +15,27 @@ $(function() {
 
         let response = await addMessage(payload);
 
-        if ( response[0].ok ) {
-            const add_message_modal = bootstrap.Modal.getInstance('#add-message-modal');
-            add_message_modal.hide();
+        const form_id = form[0].id;
+        let name_input = document.querySelector(`#${form_id} #name`);
+        let body_input = document.querySelector(`#${form_id} #body`);
+        let active_input = document.querySelector(`#${form_id} #active`);
 
-            form[0].reset();
-            const form_id = form[0].id;
-            let name_input = document.querySelector(`#${form_id} #name`);
-            let body_input = document.querySelector(`#${form_id} #body`);
-            let active_input = document.querySelector(`#${form_id} #active`);
-            name_input.value = '';
-            body_input = '';
-            active_input = '';
+        const add_message_modal = bootstrap.Modal.getInstance('#add-message-modal');
+        const error_modal = new bootstrap.Modal('#error-modal', { "backdrop": true });
+        let error_message = document.querySelector(`#error-message`);
 
+        add_message_modal.hide();
+        form[0].reset();
+        name_input.value = '';
+        body_input = '';
+        active_input = '';
+
+        if ( response[0].ok === true ) {
             location.reload();
         }
         else {
-            // TODO: do something better with the error so the user knows what's wrong
-            console.log(response);
+            error_message.innerHTML = response[1].message;
+            error_modal.show();
         }
 
         return;
